7ea0003cb56e78e252e2b60ba0ef67c6e04540d9
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-22 20:47:56 +0100

    Update add comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+# Create a file in /tmp name school
 file { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
