b22653f0259977d42ec78ec3c99200b915b18e0e
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-03 16:37:27 +0100

    Add puppet file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,23 @@
+exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+}
+
+package { 'nginx':
+    ensure => "installed"
+    require => Exec['apt-get update']
+}
+
+service { 'nginx':
+   ensure => running,
+   enable => true
+}
+
+exec { 'ufw allow':
+    command => '/usr/bin/sudo /usr/sbin/ufw allow "Nginx HTTP"'
+    require => Package['nginx']
+}
+exec { 'sed header':
+    command => "/usr/bin/sudo /usr/bin/sed -i '/^\tlocation \/ {/ a\\n\t\tadd_header X-Served-By $hostname;' /etc/nginx/sites-enabled/default"
+    require => Package['nginx']
+}
+
