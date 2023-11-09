142565acdb8e09860dcbf35c3fc6976d847131aa
Youssef Benchiir <ybenchiir@gmail.com>
2023-11-09 23:03:57 +0100

    fix pip8 error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,11 +2,12 @@
 """
     The Module that queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit.
-    If no results are found for the given subreddit, the function should return None.
+    If no results are found for the given subreddit,
+    the function should return None.
 """
 
 
-def recurse(subreddit, after = None, hot_list=[]):
+def recurse(subreddit, after=None, hot_list=[]):
     """
     List containing the titles of all hot
     articles for a given subreddit.
