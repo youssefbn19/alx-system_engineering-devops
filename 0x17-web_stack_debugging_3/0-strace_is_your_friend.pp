5b01f9b3f62dc0ca2ff562c03e5f8e12c129b27f
Youssef Benchiir <ybenchiir@gmail.com>
2023-12-26 15:47:36 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# web stack debugging by fixing a line in configuration file
-exec {'config file':
-path     => ['/usr/bin', '/bin'],
-command  => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-provider => 'shell',
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,2 +0,0 @@
-# 0x17. Web stack debugging #3
-
