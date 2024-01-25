c17a90e1a9f30a4d355cb05720ce6f6fad2bee7f
Youssef Benchiir <ybenchiir@gmail.com>
2024-01-25 12:41:43 +0100

    add all files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# increase the limit of requests
+exec {'fd':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+provider => 'shell',
+}
