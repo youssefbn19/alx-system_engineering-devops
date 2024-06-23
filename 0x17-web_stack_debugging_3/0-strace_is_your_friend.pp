f09e26be9b4e16b2090e3dfa8d01e41f27b25d88
Youssef Benchiir <ybenchiir@gmail.com>
2024-06-23 14:26:14 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fixing returning a 500 error in The Apache server
-exec {'config file':
-path     => ['/usr/bin', '/bin'],
-command  => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-provider => 'shell',
-}
